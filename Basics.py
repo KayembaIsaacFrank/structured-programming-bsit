f054bd015369cf494e0c22c0c80be66a47f5d699
KayembaIsaacFrank <isaackayemba669@gmail.com>
2025-10-10 14:23:23 +0300

    Add files via upload

---
+++ Basics.py
@@ -0,0 +1,17 @@
+# <- single line comment
+"""
+Multi line comment
+
+this is another commneted line
+"""
+
+# print("this is my name") ctrl + /
+
+#python coding case (snake case)
+"""
+first_name
+isaac_kayemba
+"""
+# Indent
+first_name ="isaac"
+print(first_name)
